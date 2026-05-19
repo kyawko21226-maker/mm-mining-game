199676edc7989239fa69fd1e3a89595d52fe7df5
kyawko21226-maker <kyawko21226@gmail.com>
2026-05-19 07:08:46 +0630

    add flask port

--- bot.py
+++ bot.py
@@ -32,3 +32,16 @@ def db():
 
 # ===== RUN =====
 bot.infinity_polling()
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
