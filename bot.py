36824da3be855e28b7e1258b0482acd1e328f0a8
kyawko21226-maker <kyawko21226@gmail.com>
2026-05-19 08:30:06 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ def run_bot():
     bot.infinity_polling()
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot, daemon=True).start()
+    threading.Thread(target=run_bot).start()
 
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
